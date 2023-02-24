fa4ac8cfcd8533445af7f74c6c625bcb7bca78a1
Mehmet <rabbi.cse.sust.bd@gmail.com>
2023-02-24 22:34:07 +0600

    Add Eureka Service and integrate with customer and fraud services.

--- src/services/customer/src/main/java/work/rabbi/customer/CustomerService.java
+++ src/services/customer/src/main/java/work/rabbi/customer/CustomerService.java
@@ -23,7 +23,7 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
         // todo: check if fraud
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
