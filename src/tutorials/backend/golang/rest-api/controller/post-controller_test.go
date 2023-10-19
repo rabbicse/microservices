afef172145b6bf8dfe42913ffca6f18e4d0e2f3a
Md. Mehedi Hasan Rabbi <rabbi.se@gmail.com>
2023-10-19 16:15:08 +0600

    Implement unit test for controller with sqlite

---
+++ src/tutorials/backend/golang/rest-api/controller/post-controller_test.go
@@ -0,0 +1,54 @@
+package controller
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/rabbicse/microservices/tree/master/src/tutorials/backend/golang/rest-api/entity"
+	"github.com/rabbicse/microservices/tree/master/src/tutorials/backend/golang/rest-api/repository"
+	"github.com/rabbicse/microservices/tree/master/src/tutorials/backend/golang/rest-api/service"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	postRepo       repository.PostRepository = repository.NewSQLiteRepository()
+	postSrv        service.PostService       = service.NewPostService(postRepo)
+	postController PostController            = NewPostController(postSrv)
+)
+
+func TestAddPost(t *testing.T) {
+	// create a new HTTP POST request
+	var jsonReq = []byte(`{"title": "Test Title 008","text": "Text 008"}`)
+	req, _ := http.NewRequest("POST", "posts", bytes.NewBuffer(jsonReq))
+
+	// assign HTTP handler function: controller AddPost function
+	handler := http.HandlerFunc(postController.AddPost)
+
+	// record HTTP response
+	response := httptest.NewRecorder()
+
+	// dispatch the HTTP request
+	handler.ServeHTTP(response, req)
+
+	// add assertion on the HTTP request
+	status := response.Code
+
+	if status != http.StatusOK {
+		t.Errorf("Handler returned a wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	// decode the HTTP response
+	var post entity.Post
+	json.NewDecoder(io.Reader(response.Body)).Decode(&post)
+
+	// assert HTTP response
+	assert.NotNil(t, post.ID)
+}
+
+func TestGetPosts(t *testing.T) {
+
+}
